cf1c5754b010ec2d870769401d07a247b9c14aa4
Pokhodnia Andrew <pokhodnia.a@gmail.com>
2021-03-15 14:17:23 +0200

    Initial dist subtree commit

---
+++ precache-manifest.d587f69407facc355f36a95195a0c5b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a0415b3e69495d27c8",
+    "url": "/Contacts-app/css/app.dd774e8b.css"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/Contacts-app/img/delete.650cdc57.svg"
+  },
+  {
+    "revision": "2cf22e0e857d95ead7f8e0aea94c73b9",
+    "url": "/Contacts-app/img/undo-arrow.2cf22e0e.svg"
+  },
+  {
+    "revision": "01a358fd0a30eb43e0dafd6b85a187d7",
+    "url": "/Contacts-app/index.html"
+  },
+  {
+    "revision": "10a0415b3e69495d27c8",
+    "url": "/Contacts-app/js/app.d1e6e3de.js"
+  },
+  {
+    "revision": "c814a61af6cc4f18b956",
+    "url": "/Contacts-app/js/chunk-vendors.fdff9330.js"
+  },
+  {
+    "revision": "db308420a83ef088238107bfaf9a9c96",
+    "url": "/Contacts-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Contacts-app/robots.txt"
+  }
+]);
